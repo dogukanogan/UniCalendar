220da7dc8569070b131dbf36c57a6b119c276f58
dogukanogan <dogukanogan35@gmail.com>
2025-12-09 10:24:22 +0300

    Initial Commit

---
+++ UniCalendarTests/UniCalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniCalendarTests.swift
+//  UniCalendarTests
+//
+//  Created by Doğukan Ogan on 9.12.2025.
+//
+
+import Testing
+@testable import UniCalendar
+
+struct UniCalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
